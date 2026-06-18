e8429140003b1aa55c7b9d71abd417b42f6ba2f2
SummerRay160 <SummerRay160@users.noreply.github.com>
2026-06-18 14:47:36 +0800

    chore(release): bump version to 1.2.4
    - Update version in AssemblyInfo.cs, LumiShift.csproj
    - Add changelog entry for v1.2.4

--- LumiShift/Properties/AssemblyInfo.cs
+++ LumiShift/Properties/AssemblyInfo.cs
@@ -22,5 +22,5 @@ using System.Runtime.InteropServices;
 //      生成号
 //      修订号
 //
-[assembly: AssemblyVersion("1.2.3")]
-[assembly: AssemblyFileVersion("1.2.3")]
+[assembly: AssemblyVersion("1.2.4")]
+[assembly: AssemblyFileVersion("1.2.4")]
