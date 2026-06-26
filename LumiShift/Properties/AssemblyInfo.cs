e19c33795221e61ba6ff8a81c6b206ba356fbc71
SummerRay160 <141414769+SummerRay160@users.noreply.github.com>
2026-06-26 10:34:39 +0800

    chore(release): bump assembly version to 1.2.5

--- LumiShift/Properties/AssemblyInfo.cs
+++ LumiShift/Properties/AssemblyInfo.cs
@@ -22,5 +22,5 @@ using System.Runtime.InteropServices;
 //      生成号
 //      修订号
 //
-[assembly: AssemblyVersion("1.2.4")]
-[assembly: AssemblyFileVersion("1.2.4")]
+[assembly: AssemblyVersion("1.2.5")]
+[assembly: AssemblyFileVersion("1.2.5")]
