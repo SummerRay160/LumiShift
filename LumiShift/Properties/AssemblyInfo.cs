03313722976e4520c0880d53423730db23486e0d
SummerRay160 <141414769+SummerRay160@users.noreply.github.com>
2026-07-09 14:13:40 +0800

    chore: bump version to 1.6.1

--- LumiShift/Properties/AssemblyInfo.cs
+++ LumiShift/Properties/AssemblyInfo.cs
@@ -22,5 +22,5 @@ using System.Runtime.InteropServices;
 //      生成号
 //      修订号
 //
-[assembly: AssemblyVersion("1.6.0")]
-[assembly: AssemblyFileVersion("1.6.0")]
+[assembly: AssemblyVersion("1.6.1")]
+[assembly: AssemblyFileVersion("1.6.1")]
